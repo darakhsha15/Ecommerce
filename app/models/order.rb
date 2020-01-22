d26aef2d6eb8827d74152f94a3caf4e0cc151250
darakhsha15 <naqvi.dara15@gmail.com>
2020-01-22 17:39:42 +0530

    total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,14 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :products
   validates :order_type, presence: true
 
+  after_create :cal_total
+  private
+  def cal_total
+    self.payable_price = 0
+    self.order_products.each do |order| 
+    self.payable_price+= order.product.price * order.qty
+  end
+  update_attributes(:payable_price=>self.payable_price)
+  end 
 end
+
